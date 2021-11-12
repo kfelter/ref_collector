5fb5eb2171281e3169b5dd3f54311ace1822968e
Kyle Felter <kylerfelter@gmail.com>
2021-11-11 21:47:56 -0500

    fix info

--- handler_info.go
+++ handler_info.go
@@ -3,12 +3,11 @@ package main
 import "net/http"
 
 func infoHandler(rw http.ResponseWriter, r *http.Request) {
-	data, err := embedFS.ReadFile("embed/info.html")
+	data, err := embedFS.ReadFile("embed/info.txt")
 	if err != nil {
 		http.Error(rw, err.Error(), 500)
 		return
 	}
 	rw.WriteHeader(200)
-	rw.Header().Add("Content-Type", "text/html")
 	rw.Write(data)
 }
